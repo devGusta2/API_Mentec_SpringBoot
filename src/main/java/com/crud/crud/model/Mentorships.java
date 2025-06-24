a0a61c860fdfa78125e392dcd7c2c2cc61d4a3e6
devGusta2 <gustavo.souza2979@gmail.com>
2025-06-24 16:12:21 -0300

    Metodos getters e setters

--- src/main/java/com/crud/crud/model/Mentorships.java
+++ src/main/java/com/crud/crud/model/Mentorships.java
@@ -1,5 +1,7 @@
 package com.crud.crud.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -7,5 +9,33 @@ import jakarta.persistence.GenerationType;
 @Entity
 public class Mentorships {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String teacher;
+    private String place;
+    private String frequency;
+    private String requirements;
+    private int duration;
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setTeacher(String teacher){
+        this.teacher =  teacher;
+    }
+    public void setPlace(String place){
+        this.place = place;
+    }
+    public void setFrequency(String frequency){
+        this.frequency = frequency;
+    }
+    public void setRequirements(String requierements){
+        this.requirements = requirements;
+    }
+
 }
