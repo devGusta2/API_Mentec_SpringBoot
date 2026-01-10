a4e7e12e8ded8150af5163d86285317ddb47c848
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-01-10 11:22:02 -0300

    Removendo arquivos

--- src/main/java/com/crud/crud/repository/MentorshipsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.crud.crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.crud.crud.model.Mentorships;
-
-public interface MentorshipsRepository extends JpaRepository<Mentorships, Long> {
-    
-}
