577811745a1f1c6ab7e107b432fd8976ea7c6a4e
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-01-10 11:32:15 -0300

    Informações gerais de qualquer usuário

--- src/main/java/com/API_MENTEC_SPRINGBOOT/Model/User.java
+++ src/main/java/com/API_MENTEC_SPRINGBOOT/Model/User.java
@@ -1,10 +1,34 @@
 package com.API_MENTEC_SPRINGBOOT.Model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_user")
+@Getter
+@Setter
 public class User {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String senha;
+    
+    @Column(unique = true)
+    private String cpf;
+
     
 }
