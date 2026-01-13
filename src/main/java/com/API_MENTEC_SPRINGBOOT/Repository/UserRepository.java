d84f2beae92f142052d1632ceef1b28103bd52cb
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-01-12 23:19:35 -0300

    Repository do usuario

---
+++ src/main/java/com/API_MENTEC_SPRINGBOOT/Repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.API_MENTEC_SPRINGBOOT.Repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.API_MENTEC_SPRINGBOOT.Model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository <User, UUID>{
+    
+}
