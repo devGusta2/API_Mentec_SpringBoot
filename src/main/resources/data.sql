8153311518ba75ed02efb83158780b165dbbd641
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-01-18 00:16:38 -0300

    Inserindo roles

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_roles (id, role_name) VALUES (1, 'BASICO');   
+INSERT INTO tb_roles (id, role_name) VALUES (2, 'ESTUDANTEFATEC');
+INSERT INTO tb_roles (id, role_name) VALUES (3, 'PROFESSOR');
+INSERT INTO tb_roles (id, role_name) VALUES (4, 'ADMIN');
+
