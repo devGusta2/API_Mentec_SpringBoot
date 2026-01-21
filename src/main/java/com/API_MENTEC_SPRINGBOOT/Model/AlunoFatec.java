2f9751e8935789fb5daa77b4dba6ba0de8ff7a3b
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-01-21 19:37:13 -0300

    Update AlunoFatec.java

--- src/main/java/com/API_MENTEC_SPRINGBOOT/Model/AlunoFatec.java
+++ src/main/java/com/API_MENTEC_SPRINGBOOT/Model/AlunoFatec.java
@@ -5,7 +5,9 @@ import com.API_MENTEC_SPRINGBOOT.Enum.Periodo;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -18,4 +20,6 @@ public class AlunoFatec extends User{
     private String ra;
     private String curso;
     private Periodo periodo;
+
+    
 }
