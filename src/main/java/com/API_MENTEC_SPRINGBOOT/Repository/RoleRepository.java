247ce904bc9e8311124bc9dddc07edf6b154cc1d
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-01-24 13:45:27 -0300

    Renomeando método de findybyname

--- src/main/java/com/API_MENTEC_SPRINGBOOT/Repository/RoleRepository.java
+++ src/main/java/com/API_MENTEC_SPRINGBOOT/Repository/RoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.API_MENTEC_SPRINGBOOT.Model.Role;
 
 public interface RoleRepository extends JpaRepository <Role, Long>{
-    
+    Role findByName(String name);
 }
