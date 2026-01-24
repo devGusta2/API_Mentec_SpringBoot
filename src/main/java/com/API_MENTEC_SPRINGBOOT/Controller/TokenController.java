c98a963c3befbe9e21e3da60993f6368c1a78689
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-01-24 00:23:06 -0300

    Ajustando controller

--- src/main/java/com/API_MENTEC_SPRINGBOOT/Controller/TokenController.java
+++ src/main/java/com/API_MENTEC_SPRINGBOOT/Controller/TokenController.java
@@ -1,14 +1,50 @@
 package com.API_MENTEC_SPRINGBOOT.Controller;
 
+import java.time.Instant;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.API_MENTEC_SPRINGBOOT.DTO.LoginRequest;
+import com.API_MENTEC_SPRINGBOOT.DTO.LoginResponse;
+import com.API_MENTEC_SPRINGBOOT.Repository.UserRepository;
 
 @RestController
 public class TokenController {
     private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public TokenController(JwtEncoder jwtEncoder){
+    public TokenController(JwtEncoder jwtEncoder, UserRepository userRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        var user = userRepository.findByEmail(loginRequest.email());
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, bCryptPasswordEncoder)) {
+            throw new BadCredentialsException("Usuario ou senha inválidos");
+        }
+
+        var expiresIn = 300L;
+        var now = Instant.now();
+        var claim = JwtClaimsSet.builder()
+                .issuer("mybackend")
+                .subject(user.get().getId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .build();
+        
+
+        var jwtValue = "";
+
+        return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
+    }
 }
