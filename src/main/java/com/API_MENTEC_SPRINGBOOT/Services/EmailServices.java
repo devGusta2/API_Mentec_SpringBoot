1c55265d73611eef8ffb37fd92cb63ef3ac40e47
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-01-29 19:02:38 -0300

    Service de emails

--- src/main/java/com/API_MENTEC_SPRINGBOOT/Services/EmailServices.java
+++ src/main/java/com/API_MENTEC_SPRINGBOOT/Services/EmailServices.java
@@ -1,8 +1,55 @@
 package com.API_MENTEC_SPRINGBOOT.Services;
 
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Service
 public class EmailServices {
-    
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String remetente;
+
+    public String enviarEmailTexto(String destinatario, String assunto, String mensagem){
+        try{
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setFrom(remetente);
+            simpleMailMessage.setTo(destinatario);
+            simpleMailMessage.setSubject(assunto);
+            simpleMailMessage.setText(mensagem);
+            javaMailSender.send(simpleMailMessage);
+            return "E-mail enviado";
+        
+            
+        } catch (Exception e) {
+            return "Falha ao enviar E-mail " + e.getLocalizedMessage();
+        }
+    }
+
+    public String gerarCodigo(){
+        return String.valueOf(100000 + new Random().nextInt());
+    }
+    public String enviarEmailParaConfirmação(String destinatario, String assunto, String mensagem){
+     try{
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setFrom(remetente);
+            simpleMailMessage.setTo(destinatario);
+            simpleMailMessage.setSubject(assunto);
+            simpleMailMessage.setText(mensagem);
+            javaMailSender.send(simpleMailMessage);
+            return "E-mail enviado";
+        
+            
+        } catch (Exception e) {
+            return "Falha ao enviar E-mail " + e.getLocalizedMessage();
+        }
+    }
 }
